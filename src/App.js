605d53dba2495c342cd359f4e08b456b18a9ac7b
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2022-12-02 12:31:30 +0530

    this push should not invoke any actions [skip actions]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    // added another comment
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
